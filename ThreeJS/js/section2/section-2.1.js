bc7a855d5ee4aebe98e25e7a3775f0d03f787ec4
Sigoway <Sigoway@hotmail.com>
2016-11-15 18:20:39 +0800

    ThreeJS之增加雾化效果，随机显示/隐藏立方体

--- ThreeJS/js/section2/section-2.1.js
+++ ThreeJS/js/section2/section-2.1.js
@@ -1,6 +1,7 @@
 ///<reference path="typings/threejs/three.d.ts" />
 
 var numberOfObjects = 0;
+var numberOfCubes = 0;
 
 var stats = initStat();
 
@@ -18,14 +19,23 @@ function initStat(){
 var controls = new function(){
     this.rotationSpeed = 0.02;
     this.boncingSpeed = 0.03;
+    this.fogDensity = 0.015
 }
 
 var gui = new dat.GUI();
 gui.add(controls, 'rotationSpeed', 0, 0.5);
 gui.add(controls, 'boncingSpeed', 0, 0.5);
+gui.add(controls, 'fogDensity', 0, 1);
 
 
 var scene = new THREE.Scene();
+//给场景设置雾化效果
+//scene.fog = new THREE.Fog(0xffffff, 0.015, 100);
+//雾化方式2
+//scene.fog = new THREE.FogExp2(0xffffff, controls.fogDensity);
+//强制场景内所有物体使用相同的材质
+//scene.overrideMaterial = new THREE.MeshLambertMaterial({color:0xff0000, wireframe: true});
+
 var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
 renderer.setClearColor('#000000', 1);
@@ -59,11 +69,11 @@ camera.lookAt(scene.position);
 
 this.addCube = function(){
     var cubeSize = Math.ceil(Math.random() * 3);
-    var cubeGeometry = new THREE.CubeGeometry(cubeSize, cubeSize, cubeSize);
+    var cubeGeometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
     var cubeMaterial = new THREE.MeshLambertMaterial({ color:Math.random() * 0xffffff });
     var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
     cube.castShadow = true;
-    cube.name = "cube_" + scene.children.length;
+    cube.name = "cube_" + (++numberOfCubes);
     cube.position.x = -30 + Math.round(Math.random() * planeGeometry.parameters.width);
     cube.position.y = Math.round(Math.random() * 5);
     cube.position.z = -20 + Math.round(Math.random() * planeGeometry.parameters.height);
@@ -77,6 +87,7 @@ this.removeCube = function(){
     var lastObject = allChildren[allChildren.length - 1];
     if(lastObject instanceof THREE.Mesh){
         scene.remove(lastObject);
+        this.numberOfCubes--;
         this.numberOfObjects = scene.children.length;
     }
 }
@@ -88,10 +99,12 @@ this.outputObjects = function(){
 $('#WebGL-output').append(renderer.domElement);
 render();
 
+var hiddenCubes = 0;
+
 function render(){
     stats.begin();
 
-    if(this.numberOfObjects < 100){
+    if(this.numberOfCubes <= 100){
         addCube();
     }
 
@@ -102,6 +115,29 @@ function render(){
             e.rotation.z += controls.rotationSpeed;
         }
     });
+
+    var cubeName = 'cube_' + Math.floor(Math.random() * 100);
+    //cubeName = 'cube_2';
+    var cubeMesh = scene.getObjectByName(cubeName);
+    if(cubeMesh != null && cubeMesh instanceof THREE.Mesh) {
+        //设置size不起作用
+        // cubeMesh.geometry.normalsNeedUpdate = false;
+        // cubeMesh.geometry.width += 1;
+        // cubeMesh.geometry.height += 1;
+        // cubeMesh.geometry.depth += 1;
+        // cubeMesh.geometry.normalsNeedUpdate = true;
+
+        if(this.hiddenCubes >= 90){
+            cubeMesh.visible = true;
+            this.hiddenCubes--;
+        } else {
+            cubeMesh.visible = false;
+            this.hiddenCubes++;
+        }
+    }
+
+    //雾化方式
+    //scene.fog = new THREE.Fog(0xffffff, controls.fogDensity, 100);
 
     requestAnimationFrame(render);
     renderer.render(scene, camera);
